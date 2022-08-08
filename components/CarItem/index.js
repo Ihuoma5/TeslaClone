b7260fd756250aecba46fa30832d4a963e73781d
Ihuoma Ndukwe <chineme4g@gmail.com>
2022-08-08 23:24:32 +0100

    Button Component

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -1,20 +1,36 @@
-import { View, Text, ImageBackground } from 'react-native'
-import React from 'react'
-import styles from './styles'
+import { View, Text, ImageBackground } from "react-native";
+import React from "react";
+import StyledButton from "../StyledButton";
+import styles from "./styles";
 
 const CarItem = () => {
   return (
     <View style={styles.carContainer}>
-    <ImageBackground
-      source={require("../../assets/images/ModelS.jpeg")}
-      style={styles.image}
-    />
-    <View style={styles.titles}>
-      <Text style={styles.title}>Model S</Text>
-      <Text style={styles.subtitle}>Starting at $69,420</Text>
-    </View>
-  </View>
-  )
-}
+      <ImageBackground
+        source={require("../../assets/images/ModelS.jpeg")}
+        style={styles.image}
+      />
+      <View style={styles.titles}>
+        <Text style={styles.title}>Model S</Text>
+        <Text style={styles.subtitle}>Starting at $69,420</Text>
+      </View>
 
-export default CarItem
+      <StyledButton
+        type="primary"
+        content={"Custom Order"}
+        onPress={() => {
+          console.warn("Pressed");
+        }}
+      />
+      <StyledButton
+        type="secondary"
+        content={"Existing inventory"}
+        onPress={() => {
+          console.warn("Pressed2");
+        }}
+      />
+    </View>
+  );
+};
+
+export default CarItem;
